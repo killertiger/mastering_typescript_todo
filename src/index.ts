6149f39adfe82706311051e095599dd9554e5e01
Marcus <moliveira@pinterest.com>
2024-12-23 03:40:42 -0800

    feat: :sparkles: Form with checkbox - TODO list page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("#todoform")! as HTMLFormElement;
+const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
-    console.log("SUBMITTED!");
+    const newTodoText = input.value;
+    const newLi = document.createElement("LI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    list.append(newLi);
+    input.value = "";
 }
 
 form.addEventListener("submit", handleSubmit);
