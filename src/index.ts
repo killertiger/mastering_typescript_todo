2df30ca9f51bb627a76efe55cf3d74b6cb18f406
Marcus <moliveira@pinterest.com>
2024-12-23 03:50:12 -0800

    feat: :sparkles: Add type todo

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,33 @@ const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("#todoform")! as HTMLFormElement;
 const list = document.getElementById("todolist")!;
 
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
 function handleSubmit(e: SubmitEvent) {
-    e.preventDefault();
-    const newTodoText = input.value;
-    const newLi = document.createElement("LI");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    newLi.append(newTodoText);
-    newLi.append(checkbox);
-    list.append(newLi);
-    input.value = "";
+  e.preventDefault();
+
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLi = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLi.append(todo.text);
+  newLi.append(checkbox);
+  list.append(newLi);
 }
 
 form.addEventListener("submit", handleSubmit);
